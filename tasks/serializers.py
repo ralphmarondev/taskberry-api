92b2f2afad2dcc230ac16e2620d7643d435496b0
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-05-31 21:02:43 +0800

    updated serializers.py to format date in a better way

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import routers, serializers, viewsets
 from .models import Task
 
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    # formatting the date
+    created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
+
     class Meta:
         model = Task
         fields = ['id', 'title', 'description', 'completed', 'created_at']
