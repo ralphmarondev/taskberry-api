e0ac63941c01d3db79bf7f78a4fc0985278d9141
Ralph Maron Eda <edaralphmaron@gmail.com>
2024-06-23 14:24:03 +0800

    refactor views.py

--- tasks/views.py
+++ tasks/views.py
@@ -1,30 +1,3 @@
-# from rest_framework.parsers import JSONParser
-# from django.views.decorators.csrf import csrf_exempt
-# from django.http import HttpResponse, JsonResponse
-# from .serializers import TaskSerializer
-# from .models import Task
-
-# @csrf_exempt
-# def tasks(request):
-#     if request.method == 'GET':
-#         return list_tasks(request)
-#     elif request.method == 'POST':
-#         return create_task(request)
-
-
-# def list_tasks(request):
-#     tasks = Task.objects.all()
-#     serializer = TaskSerializer(tasks, many=True)
-#     return JsonResponse(serializer.data, safe=False)
-
-# def create_task(request):
-#     data = JsonResponse().parse(request)
-#     serializer = TaskSerializer(data  = data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return JsonResponse(serializer.data, status=201)
-#     return JsonResponse(serializer.errors, status=400)
-
 from rest_framework import viewsets
 from .models import Task 
 from .serializers import TaskSerializer
